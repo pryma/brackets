b0abc348ca57f2983d61d13212485e3e737583d9
Maksim Pryma <Maksim.Pryma@standardbank.co.za>
2020-03-09 21:57:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let openClose = new Map();
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        openClose.set(bracketsConfig[i][0], bracketsConfig[i][1]);
+    }
+
+    let arr = str.split('');
+
+    let check = new Array();
+    for (let i of arr) {
+        let checkItem = check.pop();
+        if (checkItem != undefined) {
+            let close = openClose.get(checkItem);
+            if (close != i) {
+                check.push(checkItem)
+                check.push(i);
+            }
+        } else {
+            check.push(i);
+        }
+        
+    }
+    return check.length == 0 ? true : false;
 }
